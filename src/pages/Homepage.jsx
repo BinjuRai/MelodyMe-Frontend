611dbaa48c170dc06f2a8707b4b2e72c22abf9e1
BinjuRai <binjurai7@gmail.com>
2025-06-12 22:50:13 +0545

    changes

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,7 +3,7 @@ export default function Homepage() {
   return (
     <div className="min-h-screen">
       <div className="container text-center mt-4">
-this is my homepage
+         this is my homepage
       </div>
     </div>
   )
--- src/pages/StateManage.jsx
+++ src/pages/StateManage.jsx
@@ -2,32 +2,31 @@
 import React ,{ useEffect, useState }from 'react'
 
 export default function StateManage() {
-    //state in function component using usestate
+    
     const [data,setData] = useState("Default data")
-    //{variable, update function}
+  
     const [num, setNum] = useState(0)
    
     const updateCurrentData = () =>{
         setData("New data")
     }
-    //use Effect, dependencies checker
-    //1. Initially logic (Api calls)
+  
     useEffect(
         ()=> {
             setData("Initial Data Change")
         },
-        [] // empty for initial run
+        [] 
     )
-    //2. State dependencies 
+ 
     useEffect(
         () => {
             if(data =="Aarya"){
                 setNum(1000)
             }
         },
-        [data] //list of state
+        [data]
     )
-    //Login num < 0 data less else more
+
     useEffect(
         ()=>{
             if(num < 0){
